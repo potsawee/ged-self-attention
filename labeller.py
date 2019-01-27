2d9c2ff6cee72286970109aeac7e2ac94a125657
Potsawee Manakul <m.potsawee@gmail.com>
2019-01-27 18:30:00 +0000

    train on clc - no dropout

--- labeller.py
+++ labeller.py
@@ -5,6 +5,7 @@ import numpy as np
 import pdb
 
 # credit: https://github.com/Kyubyong/transformer
+# http://jalammar.github.io/illustrated-transformer/
 
 '''
 Self-Attention Neural Network for Sequence Labelling Tasks
@@ -26,11 +27,13 @@ class Labeller(object):
         self.label_ids = tf.placeholder(tf.int32, [None, None], name="label_ids")
         self.sent_lengths = tf.placeholder(tf.int32, [None], name="sent_lengths")
 
+        s = tf.shape(self.word_ids)
+
         # embeddings
         word_embedded = embedding(self.word_ids, self.vocab_size, self.num_units)
 
         # positional encoding
-        positional_enc = positional_encoding([self.batch_size, self.max_sentence_length], self.num_units)
+        positional_enc = positional_encoding([s[0], self.max_sentence_length], self.num_units)
 
         # inputs into the neural network
         inputs = word_embedded + positional_enc
@@ -38,60 +41,65 @@ class Labeller(object):
         if self.num_stacks > 1:
             for n in range(self.num_stacks - 1):
                 scope_name = "self_attn" + str(n)
-                inputs = self_attention_layer(inputs, 512, 8, 0.1, True, scope_name)
+                inputs = self_attention_layer(inputs, self.sent_lengths, 512, 8, 0.0, True, scope_name)
 
         scope_name = "self_attn" + str(self.num_stacks-1)
-        self_attn_outputs = self_attention_layer(inputs, 512, 8, 0.1, True, scope_name) # shape = [batch_size, max_sentence_length, num_units]
+        self_attn_outputs = self_attention_layer(inputs, self.sent_lengths, 512, 8, 0.0, True, scope_name) # shape = [batch_size, max_sentence_length, num_units]
 
 
         # ---------- linear & softmax --------- #
         with tf.variable_scope("linear_projection", reuse=None):
-            w1 = tf.get_variable("w1", shape=[512, 100], initializer=tf.glorot_normal_initializer())
-            b1 = tf.get_variable("b1", shape=[1, 100], initializer=tf.zeros_initializer())
-            w2 = tf.get_variable("w2", shape=[100, 2], initializer=tf.glorot_normal_initializer())
+            w1 = tf.get_variable("w1", shape=[512, 200], initializer=tf.glorot_normal_initializer())
+            b1 = tf.get_variable("b1", shape=[1, 200], initializer=tf.zeros_initializer())
+            w2 = tf.get_variable("w2", shape=[200, 2], initializer=tf.glorot_normal_initializer())
             b2 = tf.get_variable("b2", shape=[1, 2], initializer=tf.zeros_initializer())
 
-        x0 = tf.reshape(self_attn_outputs, shape=[self.batch_size*self.max_sentence_length, 512])
+        x0 = tf.reshape(self_attn_outputs, shape=[s[0]*self.max_sentence_length, 512])
         a1 = tf.nn.relu(tf.matmul(x0, w1) + b1)
         logits = tf.matmul(a1, w2) + b2
 
-        logits = tf.reshape(logits, shape=[self.batch_size, self.max_sentence_length, 2])
+        self.logits = tf.reshape(logits, shape=[s[0], self.max_sentence_length, 2])
 
-        self.probabilities = tf.nn.softmax(logits)
+
+        self.probabilities = tf.nn.softmax(self.logits)
         self.predictions = tf.argmax(self.probabilities, 2)
 
         # ----------- cost function ----------- #
-        crossent = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=self.label_ids)
+        crossent = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=self.logits, labels=self.label_ids)
         target_weights = tf.sequence_mask(lengths=self.sent_lengths,
                                             maxlen=self.max_sentence_length,
                                             dtype=tf.float32)
 
-        self.loss = tf.reduce_sum(crossent * target_weights) / self.batch_size
+        self.crossent_masked = crossent * target_weights
+        self.loss = tf.reduce_sum(self.crossent_masked) / self.batch_size
 
 
         # ----- Gradient and Optimisation ----- #
         trainable_params = tf.trainable_variables() # return a list of Variable objects
         gradients = tf.gradients(self.loss, trainable_params)
-        max_gradient_norm = 1.0 # set to value like 1.0, 5.0
-        clipped_gradients, _ = tf.clip_by_global_norm(gradients, max_gradient_norm)
+        # max_gradient_norm = 1.0 # set to value like 1.0, 5.0
+        # clipped_gradients, _ = tf.clip_by_global_norm(gradients, max_gradient_norm)
 
         # optimisation
         self.optimizer = tf.train.AdamOptimizer(self.learning_rate)
-        self.train_op = self.optimizer.apply_gradients(zip(clipped_gradients, trainable_params))
+        # self.optimizer = tf.train.AdadeltaOptimizer(self.learning_rate)
+        # self.train_op = self.optimizer.apply_gradients(zip(clipped_gradients, trainable_params))
+        self.train_op = self.optimizer.apply_gradients(zip(gradients, trainable_params))
 
 
         print("build neural network model successful")
 
 
-
 # -------------- Modules high-level --------------- #
-def self_attention_layer(inputs, num_units=512, num_heads=8, dropout=0.0, is_training=True,
+def self_attention_layer(inputs, input_lengths, num_units=512, num_heads=8,
+                         dropout=0.0, is_training=True,
                          scope="self_attention_layer"):
 
     with tf.variable_scope(scope):
-        attn = multihead_attention(query=inputs, key=inputs, value=inputs,
-                            num_units=num_units, num_heads=num_heads, dropout=dropout,
-                            is_training=is_training, causality=False,
+        attn = multihead_attention(inputs=inputs, input_lengths=input_lengths,
+                            num_units=num_units, num_heads=num_heads,
+                            dropout=dropout, is_training=is_training,
+                            # causality=False,
                             scope="multihead_attention", reuse=None)
 
         ffn = feedforward(attn, num_units=2048, d_model=num_units, scope="feedforward", reuse=None)
@@ -166,28 +174,30 @@ def positional_encoding(inputs_shape, num_units, zero_pad=True, scale=True,
         return outputs
 
 
-def multihead_attention(query, key, value,
-                        num_units=None, num_heads=8, dropout=0.0,
-                        is_training=True, causality=False,
+def multihead_attention(inputs, input_lengths, num_units=None, num_heads=8,
+                        dropout=0.0, is_training=True,
+                        # causality=False,
                         scope="multihead_attention", reuse=None):
 
     """Multihead Attention sub-layer
     Args:
-        query: 3d Tensor
-        key: 3d Tensor
-        value: 3d Tensor ---- self-attention value = key
+        inputs: shape = [batch_size (N), max_time_steps (T), vector_size]
+        input_lengths: shape = [batch_size] # e.g. len of each sentence in the batch
         num_units: d_model let's call it C
         num_heads: h
     """
 
     with tf.variable_scope(scope, reuse=reuse):
         if num_units == None:
-            num_units = query.get_shape().as_list()[-1]
+            num_units = inputs.get_shape().as_list()[-1]
 
         # linear projections --- output dimension [N, timestep, C]
-        Q = tf.layers.dense(query, num_units, activation=tf.nn.relu, name="query")
-        K = tf.layers.dense(key, num_units, activation=tf.nn.relu, name="key")
-        V = tf.layers.dense(value, num_units, activation=tf.nn.relu, name="value")
+        # Q = tf.layers.dense(inputs, num_units, activation=tf.nn.relu, name="query")
+        # K = tf.layers.dense(inputs, num_units, activation=tf.nn.relu, name="key")
+        # V = tf.layers.dense(inputs, num_units, activation=tf.nn.relu, name="value")
+        Q = tf.layers.dense(inputs, num_units, activation=None, use_bias=False, name="query")
+        K = tf.layers.dense(inputs, num_units, activation=None, use_bias=False, name="key")
+        V = tf.layers.dense(inputs, num_units, activation=None, use_bias=False, name="value")
 
         # split and combine multihead --- output dimension [h*N, timestep,  C/h]
         _Q = tf.concat(tf.split(Q, num_heads, axis=-1), axis=0)
@@ -196,44 +206,47 @@ def multihead_attention(query, key, value,
 
         # scoring - matrix multiplication & scaling
         scores = tf.matmul(_Q, tf.transpose(_K, perm=[0, 2, 1]))
-        scores = scores / (math.sqrt(num_units/num_heads))
+        scores = scores / (math.sqrt(num_units/num_heads)) # shape = (h*N, T, T)
 
         # scoring - key masking
-        key_masks = tf.sign(tf.reduce_sum(tf.abs(key), axis=-1))
-        key_masks = tf.tile(key_masks, [num_heads, 1])
-        key_masks = tf.tile(tf.expand_dims(key_masks, 1), [1, tf.shape(query)[1], 1])
+        max_length = inputs.get_shape().as_list()[1]
+        key_masks = tf.sequence_mask(lengths=input_lengths, maxlen=max_length, dtype=tf.bool) # (N, T)
+        key_masks = tf.tile(key_masks, [num_heads, 1]) # (h*N, T)
+        key_masks = tf.tile(tf.expand_dims(key_masks, 1), [1, tf.shape(inputs)[1], 1]) # (h*N, T, T)
+
         paddings = tf.ones_like(scores)*(-2**32+1)
-        scores = tf.where(tf.equal(key_masks, 0), paddings, scores)
+        scores = tf.where(key_masks, scores, paddings)
 
         # scoring - Causality = Future blinding
-        if causality:
-            diag_vals = tf.ones_like(scores[0, :, :])
-            tril = tf.linalg.LinearOperatorLowerTriangular(diag_vals).to_dense()
-            masks = tf.tile(tf.expand_dims(tril, 0), [tf.shape(scores)[0], 1, 1])
-
-            paddings = tf.ones_like(masks)*(-2**32+1)
-            scores = tf.where(tf.equal(masks, 0), paddings, scores)
+        # if causality:
+        #     diag_vals = tf.ones_like(scores[0, :, :])
+        #     tril = tf.linalg.LinearOperatorLowerTriangular(diag_vals).to_dense()
+        #     masks = tf.tile(tf.expand_dims(tril, 0), [tf.shape(scores)[0], 1, 1])
+        #
+        #     paddings = tf.ones_like(masks)*(-2**32+1)
+        #     scores = tf.where(tf.equal(masks, 0), paddings, scores)
 
         # scoring - softmax --- output dimension [h*N, timestep_Q, timestep_K]
         scores = tf.nn.softmax(scores)
 
         # Query Masking
-        query_masks = tf.sign(tf.reduce_sum(tf.abs(query), axis=-1))
-        query_masks = tf.tile(query_masks, [num_heads, 1])
-        query_masks = tf.tile(tf.expand_dims(query_masks, -1), [1, 1, tf.shape(key)[1]])
-        scores = scores * query_masks # broadcasting. (N, T_q, C)
+        # query_masks = tf.sequence_mask(lengths=input_lengths, maxlen=max_length, dtype=tf.float32) # (N, T)
+        # query_masks = tf.tile(query_masks, [num_heads, 1])
+        # query_masks = tf.tile(tf.expand_dims(query_masks, -1), [1, 1, tf.shape(inputs)[1]])
+        #
+        # scores = scores * query_masks # broadcasting. (N, T, C)
 
         # dropout
         scores = tf.layers.dropout(scores, rate=dropout, training=tf.convert_to_tensor(is_training))
 
         # outputs!!!
-        outputs = tf.matmul(scores, _V) # ( h*N, timestep_q, C/h)
+        outputs = tf.matmul(scores, _V) # ( h*N, T, C/h)
 
         # get the original shape back
-        outputs = tf.concat(tf.split(outputs, num_heads, axis=0), axis=-1) # (N, timestep_Q, C)
+        outputs = tf.concat(tf.split(outputs, num_heads, axis=0), axis=-1) # (N, T, C)
 
         # residual connection
-        outputs = add_residual(outputs, query)
+        outputs = add_residual(outputs, inputs)
 
         # layer normalisation
         outputs = layer_norm(outputs)
@@ -242,24 +255,28 @@ def multihead_attention(query, key, value,
 
 def feedforward(inputs, num_units=2048, d_model=512, scope="feedforward", reuse=None):
     with tf.variable_scope(scope, reuse=reuse):
-        s = inputs.get_shape().as_list()
-        x0 = tf.reshape(inputs, [s[0]*s[1], s[2]])
+        # s = inputs.get_shape().as_list()
+        # x0 = tf.reshape(inputs, [s[0]*s[1], s[2]])
 
         # layer 1
-        w1 = tf.Variable(tf.random_normal([d_model, num_units], stddev=0.01), name="w1")
-        b1 = tf.Variable(tf.constant(0.0, shape=(1, num_units)), name="b1")
-        a1 = tf.nn.relu(tf.matmul(x0,w1) + b1)
+        # w1 = tf.Variable(tf.random_normal([d_model, num_units], stddev=0.01), name="w1")
+        # b1 = tf.Variable(tf.constant(0.0, shape=(1, num_units)), name="b1")
+        # a1 = tf.nn.relu(tf.matmul(x0,w1) + b1)
+
+        a1 = tf.layers.dense(inputs, num_units, activation=tf.nn.relu, use_bias=True, name="block1")
 
         # layer 2
-        w2 = tf.Variable(tf.random_normal([num_units, d_model], stddev=0.01), name="w2")
-        b2 = tf.Variable(tf.constant(0.0, shape=(1, d_model)), name="b2")
-        z2 = tf.matmul(a1,w2) + b2
+        # w2 = tf.Variable(tf.random_normal([num_units, d_model], stddev=0.01), name="w2")
+        # b2 = tf.Variable(tf.constant(0.0, shape=(1, d_model)), name="b2")
+        # z2 = tf.matmul(a1,w2) + b2
+        z2 = tf.layers.dense(a1, d_model, activation=None, use_bias=True, name="block2")
 
         # residual connection
-        outputs = z2 + x0
+        # outputs = add_residual(z2, x0)
+        outputs = add_residual(z2, inputs)
 
         # get the original shape back
-        outputs = tf.reshape(outputs, [s[0], s[1], s[2]])
+        # outputs = tf.reshape(outputs, [s[0], s[1], s[2]])
 
         # layer normalisation
         outputs = layer_norm(outputs)
--- train.py
+++ train.py
@@ -10,12 +10,12 @@ from helper import load_vocab, load_ged_data, construct_training_data_batches
 
 def train(config):
     batches, vocab_size, word2id = construct_training_data_batches(config)
+    id2word = list(word2id.keys())
 
     config['vocab_size'] = vocab_size
 
     model = Labeller(config)
     model.build_netowrk()
-
 
     if config['use_gpu']:
         if 'X_SGE_CUDA_DEVICE' in os.environ:
@@ -36,13 +36,16 @@ def train(config):
         os.environ['CUDA_VISIBLE_DEVICES'] = ''
         sess_config = tf.ConfigProto()
 
+    sys.stdout.flush()
 
     with tf.Session(config=sess_config) as sess:
         sess.run(tf.global_variables_initializer())
 
-        # tf_variables = tf.trainable_variables()
-        # for i in range(len(tf_variables)):
-        #     print(tf_variables[i])
+        tf_variables = tf.trainable_variables()
+        for i in range(len(tf_variables)):
+            print(tf_variables[i])
+
+        # pdb.set_trace()
 
         num_epochs = config['num_epochs']
         for epoch in range(num_epochs):
@@ -55,33 +58,46 @@ def train(config):
                             model.label_ids: batch['label_ids'],
                             model.sent_lengths: batch['sent_lengths']}
 
+                # ------ debugging ------ #
+                # logits, crossent_masked = sess.run([model.logits, model.crossent_masked], feed_dict=feed_dict)
+                # pdb.set_trace()
+                # ----------------------- #
+
+
                 _, loss = sess.run([model.train_op, model.loss], feed_dict=feed_dict)
                 total_loss += loss
 
             print('epoch {} --- loss = {}'.format(epoch, total_loss))
 
-            my_sent = [word2id['<s>'], word2id['she'], word2id['are'], word2id['a'], word2id['student'], word2id['</s>']]
-            my_sent = [my_sent]
+            my_sent = [word2id['<s>'], word2id['i'], word2id['wants'], word2id['to'], word2id['run'], word2id['</s>']]
+            my_sent_len = len(my_sent)
+            my_sent = my_sent + [word2id['</s>']] * (config['max_sentence_length'] - my_sent_len)
+            feed_dict = {model.word_ids: [my_sent], model.sent_lengths: [my_sent_len]}
             probs = sess.run(model.probabilities, feed_dict=feed_dict)
-            for j, word in enumerate(my_sent[0]):
-                print("{} --- {}".format(word, probs[0][j]))
+
+            for j, word in enumerate(my_sent):
+                print("{:10s} --- {:.4f}".format(id2word[word], probs[0][j][0]))
+                sys.stdout.flush()
+                if j == my_sent_len-1:
+                    break
 
 def main():
     # ------- config ------- #
     config = {}
     config['learning_rate'] = 0.0001
-    config['batch_size'] = 1000
+    config['batch_size'] = 500
     config['num_stacks'] = 4
     config['num_units'] = 512
-    config['max_sentence_length'] = 8
-    # config['vocab_size'] = 38000
+    config['max_sentence_length'] = 32
     config['use_gpu'] = True
-    config['num_epochs'] = 50
+    config['num_epochs'] = 100
     # ---------------------- #
 
     # ------- paths -------- #
     config['vocab_path'] = 'lib/wlists/vocab.clc.min-count2.en'
-    config['data_path'] = 'lib/tsv/fcesplitpublic+bulats.nopunc.ged.spell.v3.tsv'
+    # config['data_path'] = 'lib/tsv/fcesplitpublic+bulats.nopunc.ged.spell.v3.tsv'
+    config['data_path'] = 'lib/tsv/fcesplitpublic+bulats+ielts+fcesplit+cpe+cae.nopunc.ged.spell.v3.tsv'
+    # config['data_path'] = 'lib/tsv/fake4.tsv'
     # ---------------------- #
 
     train(config)
